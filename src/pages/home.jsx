a5112d554bf550c746d8071fe568845d01fea848
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-01-17 23:58:23 +0530

    Fixing TextSpanninig Compo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import { motion } from "framer-motion";
 
 import "../styles/home.scss";
+import TextSpan from "../components/TextSpan.jsx";
 import gradient from "../assets/gradient.png";
 import iphone from "../assets/PhoneDemo.png";
+
+const styles = {
+  heroHeadText:
+    "font-black text-white lg:text-[78px] lg:leading-[110px] mt-2",
+  heroSubText:
+    "text-[#dfd9ff] font-medium lg:text-[35px] lg:leading-[40px]",
+};
 
 function MainComponent() {
   return (
@@ -14,10 +22,26 @@ function MainComponent() {
           initial={{ opacity: 0, x: 50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ delay: 0.5, duration: 1 }}
-        >
-          Invest IQ
+          // style={{ fontFamily: "'Noto Serif', serif", fontSize: '5.8rem', fontWeight:'600', marginLeft: '2rem'}}
+          style={{ fontFamily: "'Jacques Francois Shadow', serif", fontSize: '5.5rem', marginLeft: '2rem'}}
+          className={`${styles.heroHeadText} text-white`}
+        >         
+        <span className="text-[#fff]">
+          <TextSpan>I</TextSpan>
+          <TextSpan>N</TextSpan>
+          <TextSpan>V</TextSpan>
+          <TextSpan>E</TextSpan>
+          <TextSpan>S</TextSpan>
+          <TextSpan>T</TextSpan>
+        </span>
+        {' '}
+        <span className="text-[#fff]">
+          IQ
+        </span>
+
         </motion.h1>
       </div>
+
       <div className="image-container">
         <img src={gradient} className="gradient" alt="Background gradient" />
         <motion.img
