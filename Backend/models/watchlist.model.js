fbb214fa72b98a269c0f606caeba2f4d0d230f29
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-01-26 20:21:11 +0530

    Watchlist Model

---
+++ Backend/models/watchlist.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const watchlistSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    cryptocurrencies: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        addedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Watchlist = mongoose.model('Watchlist', watchlistSchema);
+
+export default Watchlist;
