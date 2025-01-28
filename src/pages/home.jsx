68144d5fdfdbe4c5bc1d3d855bc120ecc0014162
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-01-28 19:19:56 +0530

    Fix the Home str

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,64 +1,24 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useEffect } from 'react';
+import Home1 from '../components/LandingPage/Home1.jsx';
+import { Helmet } from 'react-helmet';
 
-import "../styles/home.scss";
-import TextSpan from "../components/TextSpan.jsx";
-import gradient from "../assets/gradient.png";
-import iphone from "../assets/PhoneDemo.png";
+const Home = () => {
 
-const styles = {
-  heroHeadText:
-    "font-black text-white lg:text-[78px] lg:leading-[110px] mt-2",
-  heroSubText:
-    "text-[#dfd9ff] font-medium lg:text-[35px] lg:leading-[40px]",
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, []);
+
+  return (
+    <>
+      <Helmet>
+        <title>Home | InvestIQ</title>
+      </Helmet>
+    
+      <div>
+        <Home1 />
+      </div>
+    </>
+  );
 };
 
-function MainComponent() {
-  return (
-    <div className="main-container">
-      <div className="text-content">
-        <motion.h1
-          className={`${styles.heroHeadText} text-white p-0.5`}
-          initial={{ opacity: 0, x: 50 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.5, duration: 1 }}
-          style={{ fontFamily: "'Noto Serif', serif", fontSize: '5.8rem', fontWeight:'600', marginLeft: '2rem'}}
-          // style={{ fontFamily: "'Jacques Francois Shadow', serif", fontSize: '5.5rem', marginLeft: '2rem'}}
-        >         
-        <span className="text-[#fff]">
-          <TextSpan>I</TextSpan>
-          <TextSpan>N</TextSpan>
-          <TextSpan>V</TextSpan>
-          <TextSpan>E</TextSpan>
-          <TextSpan>S</TextSpan>
-          <TextSpan>T</TextSpan>
-        </span>
-        {' '}
-        <span className="text-[#fff]">
-          IQ
-        </span>
-
-        </motion.h1>
-      </div>
-
-      <div className="image-container">
-        <img src={gradient} className="gradient" alt="Background gradient" />
-        <motion.img
-          src={iphone}
-          className="phone-image"
-          alt="iPhone demo"
-          initial={{ y: -10 }}
-          animate={{ y: 10 }}
-          transition={{
-            type: "smooth",
-            repeatType: "mirror",
-            duration: 2,
-            repeat: Infinity,
-          }}
-        />
-      </div>
-    </div>
-  );
-}
-
-export default MainComponent;
+export default Home;
