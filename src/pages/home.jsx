25eaedebde55ef23a68f41660782e91b9ed87633
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-14 14:06:34 +0530

    Update the str of Home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react';
-import Home1 from '../components/LandingPage/Home1.jsx';
-import Home2 from '../components/LandingPage/Home2.jsx';
-import Home3 from '../components/LandingPage/Home3.jsx';
-import Home4 from '../components/LandingPage/Home4.jsx';
-import GraphPage from '../components/LandingPage/GraphSection.jsx';
-import Team from '../components/LandingPage/TeamCard.jsx';
-import Footer from '../components/Footer.jsx';
 import { Helmet } from 'react-helmet';
+import Home1 from '../components/LandingPage/Home1';
+import Home2 from '../components/LandingPage/Home2';
+import Home3 from '../components/LandingPage/Home3';
+import Home4 from '../components/LandingPage/Home4';
+import CryptoMarketChart from '../components/LandingPage/CryptoMarketChart';
+import TeamCard from '../components/LandingPage/TeamCard';
+import Footer from '../components/Footer';
 
 const Home = () => {
-
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, []);
@@ -17,18 +16,19 @@ const Home = () => {
   return (
     <>
       <Helmet>
-        <title>Home | InvestIQ</title>
+        <title>Home | InvestIQ - Your Smart Investment Platform</title>
       </Helmet>
-    
-      <div>
+
+      <main>
         <Home1 />
         <Home2 />
         <Home3 />
         <Home4 />
-        <GraphPage />
-        <Team />
-        <Footer />
-      </div>
+        <CryptoMarketChart />
+        <TeamCard />
+      </main>
+
+      <Footer />
     </>
   );
 };
