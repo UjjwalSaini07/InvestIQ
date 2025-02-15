82f8e9f4adfb5042ad6e4194670f50f07016e3d9
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-15 22:56:05 +0530

    update the handle State

--- src/components/compare/SelectCoins.jsx
+++ src/components/compare/SelectCoins.jsx
@@ -3,11 +3,15 @@ import { Select, SelectTrigger, SelectContent, SelectItem } from "../../@/ui/sel
 import SelectDays from "./SelectDays";
 
 function SelectCoins({ allCoins, crypto1, crypto2, onCoinChange, days, handleDaysChange }) {
+  const handleCoinChange = (value, isCrypto2) => {
+    onCoinChange(value, isCrypto2);
+  };
+
   return (
     <div className="flex flex-wrap justify-start items-center gap-6 m-6">
       <div className="flex items-center gap-4">
         <p className="m-0 text-white font-semibold">Crypto 1</p>
-        <Select onValueChange={(value) => onCoinChange(value, false)} value={crypto1}>
+        <Select onValueChange={(value) => handleCoinChange(value, false)} value={crypto1}>
           <SelectTrigger
             className="w-48 h-10 text-white bg-[#2c2c2c] border border-gray-500 rounded-md pl-3 transition-all hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-accent-color"
           >
@@ -31,7 +35,7 @@ function SelectCoins({ allCoins, crypto1, crypto2, onCoinChange, days, handleDay
 
       <div className="flex items-center gap-4">
         <p className="m-0 text-white font-semibold">Crypto 2</p>
-        <Select onValueChange={(value) => onCoinChange(value, true)} value={crypto2}>
+        <Select onValueChange={(value) => handleCoinChange(value, true)} value={crypto2}>
           <SelectTrigger
             className="w-48 h-10 text-white bg-[#2c2c2c] border border-gray-500 rounded-md pl-3 transition-all hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-accent-color"
           >
