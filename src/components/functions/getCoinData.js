a82728f2c8fc0b5c58a5560fed64e1b5d50a585a
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-16 18:52:37 +0530

    Optimized these function of get coins

--- src/components/functions/getCoinData.js
+++ src/components/functions/getCoinData.js
@@ -1,19 +1,13 @@
 import axios from "axios";
 
-export const getCoinData = (id, setError) => {
-  const coin = axios
-    .get(`https://api.coingecko.com/api/v3/coins/${id}`)
-    .then((response) => {
-      if (response.data) {
-        return response.data;
-      }
-    })
-    .catch((e) => {
-      console.log(e.message);
-      if (setError) {
-        setError(true);
-      }
-    });
-
-  return coin;
+export const getCoinData = async (id, setError) => {
+  try {
+    const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}`);
+    return response.data || null; // Return data or null if no data
+  } catch (e) {
+    console.log(e.message);
+    if (setError) {
+      setError(true);
+    }
+  }
 };
