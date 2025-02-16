247e86161bef1a84933eef5aedb94fd7de9264a7
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-16 20:11:43 +0530

    Making a loader

--- src/pages/compare.jsx
+++ src/pages/compare.jsx
@@ -154,8 +154,12 @@ function Compare() {
       </div>
 
       {loading && (
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center z-50">
-          <div className="w-16 h-16 border-4 border-t-4 border-blue-500 rounded-full animate-spin"></div>
+        <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
+          <div className="relative w-16 h-16">
+            <div className="absolute inset-0 border-4 border-t-4 border-transparent border-blue-400 rounded-full animate-spin"></div>
+            <div className="absolute inset-4 border-4 border-blue-400 rounded-full"></div>
+            <div className="absolute w-4 h-4 bg-blue-400 rounded-full inset-center"></div>
+          </div>
         </div>
       )}
 
