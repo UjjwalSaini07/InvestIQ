69126cbdfc3403326f41c7ee7174fb1c3ea3fc31
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-16 15:25:37 +0530

    Enable the loading

--- src/pages/compare.jsx
+++ src/pages/compare.jsx
@@ -165,11 +165,11 @@ function Compare() {
       </div>
 
       {/* Loading Spinner */}
-      {/* {loading && (
+      {loading && (
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center z-50">
           <div className="w-16 h-16 border-4 border-t-4 border-blue-500 rounded-full animate-spin"></div>
         </div>
-      )} */}
+      )}
     </div>
   );
 }
