e8fec15d0008d78fdd09cb49978cbef1f189a8ad
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-19 00:32:16 +0530

    Contact page Integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Layout from "./components/common/Layout";
 import Header from './components/common/Header';
-import Home from './pages/Home.jsx';
-import Compare from './pages/Compare.jsx';
-import HelpCenter from './components/UserCenter/HelpCenter.jsx';
+import Home from './pages/Home';
+import Compare from './pages/Compare';
+import HelpCenter from './components/UserCenter/HelpCenter';
+import ContactUs from './components/UserCenter/ContactUs';
 import './App.scss';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/compare" element={<Compare />} />
               <Route path="/helpcenter" element={<HelpCenter />} />
+              <Route path="/contactus" element={<ContactUs />} />
             </Route>
           </Routes>
         </>
