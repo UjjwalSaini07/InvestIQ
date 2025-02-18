be5da1fb986b110a45d9f592707c331a2f0c2168
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-18 22:55:35 +0530

    Add help center

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from "./components/Layout.jsx";
 import Header from './components/header.jsx';
 import Home from './pages/Home.jsx';
+import HelpCenter from './components/UserCenter/HelpCenter.jsx';
 import Compare from './pages/Compare.jsx';
 import './App.scss';
 
@@ -16,6 +17,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Home />} />
               <Route path="/compare" element={<Compare />} />
+              <Route path="/helpcenter" element={<HelpCenter />} />
             </Route>
           </Routes>
         </>
