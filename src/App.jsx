f6dd0b519e3c268eb59f444e774bf464a26784c0
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-18 21:25:43 +0530

    Correct the Appjsx Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from "./components/Layout.jsx";
 import Header from './components/header.jsx';
 import Home from './pages/Home.jsx';
-import Compare from './pages/compare.jsx';
+import Compare from './pages/Compare.jsx';
 import './App.scss';
 
 function App() {
