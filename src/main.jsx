980cea83a43d9fb1fa35da284b8bb25a64fd1ab8
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-20 00:25:13 +0530

    Fix the route with Redux Toolkit and Make Proper Channel

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      <ToastContainer />
+    </Provider>
   </StrictMode>
-)
+);
