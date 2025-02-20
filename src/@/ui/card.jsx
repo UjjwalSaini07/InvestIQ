674c9da2a337ac620077e51d9993252a17268e49
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-20 16:47:17 +0530

    Update Card Design in ShadCN

--- src/@/ui/card.jsx
+++ src/@/ui/card.jsx
@@ -5,10 +5,11 @@ import { cn } from "../lib/utils"
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
-    {...props} />
-))
-Card.displayName = "Card"
+    className={cn("rounded-xl bg-card text-card-foreground shadow-none", className)}
+    {...props}
+  />
+));
+Card.displayName = "Card";
 
 const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
   <div
