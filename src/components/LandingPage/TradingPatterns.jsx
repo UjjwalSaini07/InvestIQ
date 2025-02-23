9db6f80b025e891204928f93f63d62a19b637bb4
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-23 17:30:20 +0530

    Update the Designing

--- src/components/LandingPage/TradingPatterns.jsx
+++ src/components/LandingPage/TradingPatterns.jsx
@@ -26,112 +26,126 @@ const data = [
     title: "Volume Footprints",
     description:
       "Displays the distribution of trading volume at various price levels for each candlestick within a specified timeframe.",
-    image: {VolumeFootprint},
+    image: VolumeFootprint,
   },
   {
     title: "Time Price Opportunity",
     description:
       "Enables traders to visualize price movements as the profile period progresses, illustrating the concentration of prices at specific levels during a given timeframe.",
-    image: {TimePriceOpportunity},
+    image: TimePriceOpportunity,
   },
   {
     title: "Session Volume Profile",
     description:
       "Calculates volume data within a specified session or sub-session, allowing traders to analyze intraday volume activity for a particular segment of the session.",
-    image: {SessionVolumeProfile},
+    image: SessionVolumeProfile,
   },
   {
     title: "Candles",
     description:
       "One of the most popular traditional charts on TradingView. Conveys the size of price movements using different colors.",
-    image: {Candles},
+    image: Candles,
   },
   {
     title: "Bars",
     description:
       "A chart, composed of multiple price bars, each visualizing the price movement of an asset or security over a specified time period.",
-    image: {Bars},
+    image: Bars,
   },
   {
     title: "Volume Candles",
     description:
       "This chart visualizes trading volume, allowing traders to assess the volume of trades using candlesticks.",
-    image: {VolumeCandles},
+    image: VolumeCandles,
   },
   {
     title: "Range",
-    description: "Enables users to analyze the price movements of financial instruments with more clarity, minimizing market noise.",
-    image: {Range},
+    description:
+      "Enables users to analyze the price movements of financial instruments with more clarity, minimizing market noise.",
+    image: Range,
   },
   {
     title: "Heikin Ashi",
-    description: "Reduces market noise by smoothing significant price swings, making it easier to identify trend movements within the market.",
-    image: {HeikinAshi},
+    description:
+      "Reduces market noise by smoothing significant price swings, making it easier to identify trend movements within the market.",
+    image: HeikinAshi,
   },
   {
     title: "Renko",
-    description: "Allows users to detect major price moves within a Renko box, if they occurred.",
-    image: {Renko},
+    description:
+      "Allows users to detect major price moves within a Renko box, if they occurred.",
+    image: Renko,
   },
   {
     title: "Line break",
-    description: "Focuses solely on price movements, ignoring time intervals within this Japanese chart style.",
-    image: {LineBreak},
+    description:
+      "Focuses solely on price movements, ignoring time intervals within this Japanese chart style.",
+    image: LineBreak,
   },
   {
     title: "Point & figure",
-    description: "Illustrates price movements as a lone metric, disregarding time intervals in their creation.",
-    image: {PointFigure},
+    description:
+      "Illustrates price movements as a lone metric, disregarding time intervals in their creation.",
+    image: PointFigure,
   },
   {
     title: "Kagi",
-    description: "Utilizes vertical lines (green for up and red for down) connected by small horizontal lines to highlight significant movements in an asset's price.",
-    image: {Kagi},
+    description:
+      "Utilizes vertical lines (green for up and red for down) connected by small horizontal lines to highlight significant movements in an asset's price.",
+    image: Kagi,
   },
   {
     title: "Area",
-    description: "Visualizes data in a similar way to a line chart. An area chart plots and connects data with the area in between filled with color to convey information.",
-    image: {Area},
+    description:
+      "Visualizes data in a similar way to a line chart. An area chart plots and connects data with the area in between filled with color to convey information.",
+    image: Area,
   },
   {
     title: "HLC area",
-    description: "Focused on three data-point areas — High-Low-Close (HLC) — to show how price changes over a period of time, allowing traders to disregard the open price value, which is less significant for analysis.",
-    image: {HLCArea},
+    description:
+      "Focused on three data-point areas — High-Low-Close (HLC) — to show how price changes over a period of time, allowing traders to disregard the open price value, which is less significant for analysis.",
+    image: HLCArea,
   },
   {
     title: "Baseline",
-    description: "Explores price movements relative to a selected baseline, giving traders the tools needed to analyze price fluctuations.",
-    image: {Baseline},
+    description:
+      "Explores price movements relative to a selected baseline, giving traders the tools needed to analyze price fluctuations.",
+    image: Baseline,
   },
   {
     title: "Line",
-    description: "Connects data points with a single line to form a classic line chart.",
-    image: {Line},
+    description:
+      "Connects data points with a single line to form a classic line chart.",
+    image: Line,
   },
   {
     title: "Step line",
-    description: "Forms a series of steps using horizontal and vertical lines to highlight the irregularity of changes within the market.",
-    image: {StepLine},
+    description:
+      "Forms a series of steps using horizontal and vertical lines to highlight the irregularity of changes within the market.",
+    image: StepLine,
   },
   {
     title: "Line with markers",
-    description: "Adds markers to the classic line charts to represent essential data points.",
-    image: {LineWithMarker},
+    description:
+      "Adds markers to the classic line charts to represent essential data points.",
+    image: LineWithMarker,
   },
   {
     title: "High-low",
     description: "Displays the range of prices from high to low on each bar.",
-    image: {HighLow},
+    image: HighLow,
   },
   {
     title: "Columns",
-    description: "The chart represents the change in the real price of an instrument through the use of columns.",
-    image: {Columns},
+    description:
+      "The chart represents the change in the real price of an instrument through the use of columns.",
+    image: Columns,
   },
   {
     title: "Hollow candles",
-    description: "This chart displays data as hollow candlesticks, similar to Japanese candlesticks, except their fill and color help to interpret data.",
-    image: {HollowCandles},
+    description:
+      "This chart displays data as hollow candlesticks, similar to Japanese candlesticks, except their fill and color help to interpret data.",
+    image: HollowCandles,
   },
 ];
 
@@ -162,9 +176,9 @@ const TradingPatterns = () => {
                     : "transform scale-100 text-gray-400"
                 }`}
               >
-                <h2 className="text-3xl font-semibold">{item.title}</h2>
+                <h2 className="text-5xl font-semibold">{item.title}</h2>
                 {index === currentIndex && (
-                  <p className="mt-2 text-sm">{item.description}</p>
+                  <p className="mt-2 text-2sm">{item.description}</p>
                 )}
               </div>
             ))}
@@ -173,18 +187,21 @@ const TradingPatterns = () => {
             <img
               src={data[currentIndex].image}
               alt={data[currentIndex].title}
-              className="rounded-xl shadow-lg max-h-full w-auto border border-gray-700"
+              className="max-h-full w-auto"
             />
           </div>
         </div>
 
-        <div className="absolute bottom-6 right-6 flex space-x-4">
+        <div className="absolute bottom-0 right-6 flex space-x-4 items-center">
           <button
             onClick={handlePrevious}
             className="w-12 h-12 flex items-center justify-center rounded-full text-lg font-bold"
           >
             {"<"}
           </button>
+          <span className="text-lg">
+            {currentIndex + 1}/{data.length}
+          </span>
           <button
             onClick={handleNext}
             className="w-12 h-12 flex items-center justify-center rounded-full text-lg font-bold"
