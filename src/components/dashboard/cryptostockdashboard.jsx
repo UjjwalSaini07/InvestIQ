d880ae9d298fd3a34febadacf9fdae6d92d2bcfe
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-03-02 00:15:18 +0530

    Making in inr from usd

--- src/components/dashboard/cryptostockdashboard.jsx
+++ src/components/dashboard/cryptostockdashboard.jsx
@@ -25,7 +25,7 @@ function CryptoStockDashboard() {
     setLoading(true);
     try {
       const response = await axios.get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false"
       );
       setCoins(response.data);
     } catch (error) {
