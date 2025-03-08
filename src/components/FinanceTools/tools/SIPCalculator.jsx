575d01073838739c024f329f323f00f322a506ae
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-03-09 00:06:34 +0530

    Design enhancements- Sip Tool

--- src/components/FinanceTools/tools/SIPCalculator.jsx
+++ src/components/FinanceTools/tools/SIPCalculator.jsx
@@ -22,8 +22,9 @@ const SIPCalculator = () => {
       <h2 className="text-2xl font-bold mb-4">SIP Lump Calculator</h2>
       <div className="space-y-4">
         <div>
-          <Label>Monthly Investment</Label>
+          <Label className="font-bold">Monthly Investment</Label>
           <Input
+            className="mt-2"
             type="number"
             value={investment}
             onChange={(e) => setInvestment(e.target.value)}
@@ -31,8 +32,9 @@ const SIPCalculator = () => {
           />
         </div>
         <div>
-          <Label>Annual Rate of Return (%)</Label>
+          <Label className="font-bold">Annual Rate of Return (%)</Label>
           <Input
+            className="mt-2"
             type="number"
             value={rate}
             onChange={(e) => setRate(e.target.value)}
@@ -40,23 +42,30 @@ const SIPCalculator = () => {
           />
         </div>
         <div>
-          <Label>Investment Duration (Years)</Label>
+          <Label className="font-bold">Investment Duration (Years)</Label>
           <Input
+            className="mt-2"
             type="number"
             value={years}
             onChange={(e) => setYears(e.target.value)}
             placeholder="Enter years"
           />
         </div>
-        <Button onClick={calculateSIP} className="w-full mt-4">
+
+        <Button
+          onClick={calculateSIP}
+          className="w-full mt-4 bg-blue-600 text-white font-semibold py-3 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300 ease-in-out transform hover:scale-[1.01]"
+        >
           Calculate
         </Button>
       </div>
+
       {result && (
-        <p className="mt-4 text-green-600 font-semibold text-lg">
+        <p className="mt-4 text-green-600 font-semibold text-lg bg-green-100 p-4 rounded-lg shadow-xl text-center">
           Future Value: ₹{result}
         </p>
       )}
+
     </div>
   );
 };
