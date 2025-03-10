fc9c8cfc6b10f5c2b8d5d81563330eba74776711
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-03-10 11:07:55 +0530

    Inject the Slider into Wrapper

--- src/components/LandingPage/HomeSections/WidgetWraper.jsx
+++ src/components/LandingPage/HomeSections/WidgetWraper.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Section1 from '../TradingWidgets/CryptoSlider';
 import Section2 from "../TradingWidgets/AdvanceTradingChart";
 import Section3 from "../TradingWidgets/StockSlider";
+import Section4 from "../TradingWidgets/IndexSlider";
 
 const WidgetWraper = () => {
   useEffect(() => {
@@ -13,6 +14,9 @@ const WidgetWraper = () => {
         <Section1/>
         <Section2 />
         <Section3 />
+        <div className="-mt-6">
+          <Section4 />
+        </div>
       </div>
   );
 };
