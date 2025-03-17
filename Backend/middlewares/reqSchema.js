8305ee725fc1f34bc8c16df709029fe1565ae110
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-03-17 13:19:10 +0530

    Temporary Block Log file

--- Backend/middlewares/reqSchema.js
+++ Backend/middlewares/reqSchema.js
@@ -1,91 +1,74 @@
-const z = require("zod");
+const { z } = require('zod');
 
 const baseSchema = z.object({
     name: z
         .string()
         .trim()
-        .min(3 , {
-            message : "Name must be atleast 3 characters long"
-        })
-        .max(200 , {
-            message : "Name must not exceed 200 characters"
-        })
+        .min(3, { message: "Name must be at least 3 characters long" })
+        .max(200, { message: "Name must not exceed 200 characters" })
         .optional(),
     email: z
         .string()
         .trim()
-        .email({
-            message : "Invalid email"
-        })
+        .email({ message: "Invalid email" })
         .optional(),
     password: z
         .string()
-        .min(6 , {
-            message : "Password must be atleast 6 characters long"
+        .min(6, { message: "Password must be at least 6 characters long" })
+        .max(1024, { message: "Password must not exceed 1024 characters" })
+        .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/, {
+            message: "Password must contain at least one uppercase letter, one lowercase letter, and one number",
         })
-        .max(1024 , {
-            message : "Password must not exceed 200 characters"
-        })
-        .regex(
-            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/,
-            {
-                message : "Password must contain atleast one uppercase letter, one lowercase letter and one number"
-            }
-        )
         .optional(),
     newPassword: z
         .string()
-        .min(6 , {
-            message : "Password must be atleast 6 characters long"
+        .min(6, { message: "Password must be at least 6 characters long" })
+        .max(1024, { message: "Password must not exceed 1024 characters" })
+        .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/, {
+            message: "Password must contain at least one uppercase letter, one lowercase letter, and one number",
         })
-        .max(1024 , {
-            message : "Password must not exceed 200 characters"
-        })
-        .regex(
-            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/,
-            {
-                message : "Password must contain atleast one uppercase letter, one lowercase letter and one number"
-            }
-        )
-        .optional()
+        .optional(),
 });
 
 const registerSchema = baseSchema.pick({
     name: true,
     email: true,
-    password: true
-})
+    password: true,
+});
 
 const loginSchema = baseSchema.pick({
     email: true,
-    password: true
-})
+    password: true,
+});
 
 const forgotPasswordSchema = baseSchema.pick({
-    email: true
-})
+    email: true,
+});
 
 const resetPasswordSchema = baseSchema.pick({
     email: true,
-    newPassword: true
-})
+    newPassword: true,
+});
 
 const validateRequest = (schema) => {
-    return async (req , res , next) => {
+    return async (req, res, next) => {
         try {
             await schema.parseAsync(req.body);
             next();
         } catch (error) {
             if (error instanceof z.ZodError) {
-                const err = error.errors.map(err => err.message).join(", ");
+                const errors = error.errors.map((err) => ({
+                    path: err.path,
+                    message: err.message,
+                }));
                 return res.status(400).json({
                     success: false,
-                    error: err
+                    errors,
                 });
             }
             return res.status(500).json({
                 success: false,
-                error: error.message
+                error: "Internal server error",
             });
         }
     };
@@ -96,5 +79,5 @@ module.exports = {
     loginSchema,
     forgotPasswordSchema,
     resetPasswordSchema,
-    validateRequest
-}
+    validateRequest,
+};
