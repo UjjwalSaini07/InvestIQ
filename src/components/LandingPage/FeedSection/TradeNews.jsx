35e82b823e021fad0b770bd00bf0fcdeabd2ef2d
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-03-28 17:59:28 +0530

    Update the Status Check

--- src/components/LandingPage/FeedSection/TradeNews.jsx
+++ src/components/LandingPage/FeedSection/TradeNews.jsx
@@ -11,7 +11,7 @@ const TradeNews = () => {
   useEffect(() => {
     const fetchNews = async () => {
       try {
-        const apiKey = import.meta.env.VITE_NEWS_API;
+        const apiKey = import.meta.env.VITE_NEWS_APIKEY;
         if (!apiKey) {
           throw new Error("API Key is missing. Please check your environment configuration.");
         }
