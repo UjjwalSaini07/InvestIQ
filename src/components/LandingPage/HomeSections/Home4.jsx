4da58d11f5697ae9c4c49795c018f2387f846b43
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-04-04 14:18:28 +0530

    Remove Unwanted CodeSet

--- src/components/LandingPage/HomeSections/Home4.jsx
+++ src/components/LandingPage/HomeSections/Home4.jsx
@@ -5,7 +5,6 @@ import "aos/dist/aos.css";
 import Section1 from "../StockSlider/Cap1Stock";
 import Section2 from "../StockSlider/Cap2Stock";
 import Section3 from "../FeedSection/TradeNews";
-// import Section3 from "../FeedSection/TryStories";
 import Section4 from "../FeedSection/TopStories";
 import Section5 from "../Geographical/WorldInflation";
 import Section6 from "./WidgetWraper";
