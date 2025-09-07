4f888abb17df5dd7d96d1f8f9c4f6709dd3156b8
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-09-07 21:43:22 +0530

    Removing the Files For Make Change in the Directory
    Co-Authored-By: Gayatri Singh <150016577+Gayatrisin123@users.noreply.github.com>

--- src/components/utils/ProtectedRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-const ProtectedRoute = ({ condition, redirectTo }) => {
-  return condition ? <Outlet /> : <Navigate to={redirectTo} />;
-};
-
-export default ProtectedRoute;
